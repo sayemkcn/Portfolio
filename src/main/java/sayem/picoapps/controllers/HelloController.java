2c98c2a327816d85f8a46acf92be9e295b7ff2f9
Sayem Hossain <sayemkcn@gmail.com>
2016-05-06 11:57:44 +0600

    moved user registration,login codes from hellocontroller to usercontroller

--- src/main/java/sayem/picoapps/controllers/HelloController.java
+++ src/main/java/sayem/picoapps/controllers/HelloController.java
@@ -20,48 +20,5 @@ import sayem.picoapps.services.UserService;
 @Controller
 public class HelloController {
 	
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private UserService userService;
 	
-	@RequestMapping(value="",method=RequestMethod.GET)
-	public String hello(){
-		return "index";
-	}
-	
-	@RequestMapping(value="/register",method=RequestMethod.GET)
-	public String registrationPage(){
-		return "user/register";
-	}
-	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public String registration(@ModelAttribute User user,BindingResult bResult,Model model){
-		if (bResult.hasErrors()) {
-			System.out.println(bResult.toString());
-		}
-		user.getRoles().add("ROLE_USER");
-		userRepository.saveAndFlush(user);
-		model.addAttribute("message","Successfully added user!");
-		return "user/register";
-	}
-	
-	@RequestMapping(value="/login",method=RequestMethod.GET)
-	public String loginPage(){
-		return "user/login";
-	}
-	
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public String login(@RequestParam Map<String, String> map,Model model,HttpSession httpSession){
-		if (userService.isUserRegistered(map.get("username"))) {
-			if (userService.isPasswordValidForUser(map.get("username"), map.get("password"))) {
-				model.addAttribute("message", "Login Successful!");
-				httpSession.setAttribute("user", userRepository.findByUsername(map.get("username")));
-			}else {
-				model.addAttribute("message","Password Wrong");
-			}
-		}else {
-			model.addAttribute("message","User Not Registered!");
-		}
-		return "user/login";
-	}
 }
